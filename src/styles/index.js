a8d3a709b0612e4666d29e5bb7af38ede0ffbce4
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-05-05 02:21:15 +0700

    add style for map and header text

--- src/styles/index.js
+++ src/styles/index.js
@@ -5,6 +5,15 @@ export const styles = StyleSheet.create({
         flex:1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    viewMap:{
+        height:250,
+        marginTop: 5,
+        // marginHorizontal: 5,
+    },
+    headerText:{
+        fontSize:20,
+        fontWeight:"400",
     }
 })
 
