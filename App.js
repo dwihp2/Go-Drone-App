fee62a4706e8650e092d1dcd19cf6aabc90d588a
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-05-05 02:18:30 +0700

    minor change

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
+import { View, Text, Stylesheet } from 'react-native';
+import { styles } from './src/styles';
 import RootNavigator from './src/rootNavigator';
 import navigationServices from './src/helper/navigationServices';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
+
+Mapbox.setAccessToken("pk.eyJ1IjoiZHdpaHAyIiwiYSI6ImNqdWRsajF0NDEwZTU0ZHBicTlsY3lyNjQifQ.hVZkan4i6qiTTh0WfVGwsg");
+//Mapbox.setAccessToken(config.get('accessToken'));
+
 
 export default class App extends Component {
   render() {
