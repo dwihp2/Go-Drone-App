66ea842bbf01da96504d21270891cf85ecb7a43e
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-05-05 02:21:57 +0700

    adding map to program

--- src/Home_Page/ReceiverScreen.js
+++ src/Home_Page/ReceiverScreen.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View, Text, Stylesheet } from 'react-native';
+import { View, Text, Stylesheet, Button } from 'react-native';
 import { styles } from '../styles';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
+
+Mapbox.setAccessToken("pk.eyJ1IjoiZHdpaHAyIiwiYSI6ImNqdWRsajF0NDEwZTU0ZHBicTlsY3lyNjQifQ.hVZkan4i6qiTTh0WfVGwsg");
 
 const ReceiverScreen = (props) => (
-    <View style = {styles.container}>
-        <Text>ReceiverScreen</Text>
+    <View style = {{flex:1}}>
+        <View style = {{alignItems:"center",justifyContent:"center",marginVertical:10}}>
+			<Text style = {styles.headerText}>Tujuan</Text>
+		</View>
+        <Mapbox.MapView
+            styleURL = {Mapbox.StyleURL.Street}
+            zoomLevel = {16}
+            centerCoordinate = {[104.04866, 1.11862]}
+            style = {styles.viewMap}>
+        </Mapbox.MapView>
     </View>
 )
 export default ReceiverScreen;
