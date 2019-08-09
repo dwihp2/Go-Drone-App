3e26e958ff01d55b3dad7c633cdb7f2ef844ddc1
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-08-09 17:51:04 +0700

    change application Logo in login page

--- src/components/LogoLogin.js
+++ src/components/LogoLogin.js
@@ -3,13 +3,15 @@ import {View, Text, Image} from 'react-native';
 
 const LogoLogin = () => (
     <View style = {{alignItems:'center', justifyContent:'center'}}>
-        <Image
-            source = {require("../images/drone.png")}
-            style = {{width:200, height:200}}
-        />
-        <Text style = {{fontSize:25, fontWeight:"bold", textAlign:"center"}}>
+        <View style = {{margin: 50}}>
+            <Image
+                source = {require('../images/godrone.png')}
+                style = {{width:260, height:65}}
+            />
+        </View>
+        {/* <Text style = {{fontSize:25, fontWeight:"bold", textAlign:"center"}}>
             {'GO DRONE APPLICATION'}
-        </Text>
+        </Text> */}
     </View>
 );
 
