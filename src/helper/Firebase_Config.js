00bb48044eaad016a727932b3cf168bd8e3b1d52
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2019-08-11 19:20:38 +0700

    Create a file for config firebase

---
+++ src/helper/Firebase_Config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDGsFlxF2OGb3Wk065n-MYXRl4GNKu6NhU",
+    authDomain: "go-drone-app-1555832629237.firebaseapp.com",
+    databaseURL: "https://go-drone-app-1555832629237.firebaseio.com",
+    projectId: "go-drone-app-1555832629237",
+    storageBucket: "",
+    messagingSenderId: "421705696625",
+    appId: "1:421705696625:web:e3c038c46e7934fc"
+};
+
+const Firebase = firebase.initializeApp(config);
+export default Firebase;
+
+// export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
+
